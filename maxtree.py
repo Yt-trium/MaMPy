bafb917de4bef25b6c8d955f977861004be0f57c
Yt-trium <cyril.meyer@etu.unistra.fr>
2018-11-17 19:15:08 +0100

    work on maxtree

--- maxtree.py
+++ maxtree.py
@@ -1,13 +1,17 @@
+import numpy as np
+
 import scipy as scp
 import scipy.misc
 
-import numpy as np
-
-
 def image_read(filename):
-    img = (scp.misc.imread(name=filename, flatten=True, mode="L")).astype(dtype=np.uint8)
-
+    return (scp.misc.imread(name=filename, flatten=True, mode="L")).astype(dtype=np.uint8)
 
 if __name__ == '__main__':
-    image_read(filename="examples/images/cameraman.jpg")
-    
+    img1 = image_read(filename="examples/images/cameraman.jpg")
+
+    print(img1)
+    print(img1.shape)
+    print(type(img1))
+    print(type(img1[0][0]))
+    print(img1.max())
+    print(img1.min())
