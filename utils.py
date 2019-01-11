ae948b24ee30bc4e61c7fe8c68f6216789d81e23
Yt-trium <cyril.meyer.68@gmail.com>
2019-01-11 14:53:49 +0100

    change comment in utils

--- utils.py
+++ utils.py
@@ -6,8 +6,8 @@ C. Meyer
 import imageio
 import numpy as np
 
-"""
- * Read an image as a 2D array of 8 bit integers (grayscale pixels)
-"""
 def image_read(filename):
+    """
+    Read an image as a 2D array of 8 bit integers (grayscale pixels)
+    """
     return imageio.imread(uri=filename, as_gray=True).astype(dtype=np.uint8)
