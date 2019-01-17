ac36ac488dea049ee02da691c0a6bee44f4d12a3
Yt-trium <cyril.meyer@etu.unistra.fr>
2019-01-17 16:51:41 +0100

    refactorization (only 2D image for now)

--- maxtree.py
+++ maxtree.py
@@ -24,100 +24,6 @@ from utils import image_read
 # Named Tuple for the max-tree structure
 MaxTreeStructure = namedtuple("MaxTreeStructure", ["parent", "S"])
 
-@jit(nopython=True)
-def maxtree_berger_rank(image, connection8=True):
-    """
-    Union-find with union-by-rank based max-tree algorithm .
-
-    -> Algorithm 3 in the paper.
-
-    Arguments:
-    image is supposed to be a numpy array.
-
-    Returns:
-    """
-
-    (height, width) = (image.shape[0], image.shape[1])
-
-    flatten_image = image.flatten()
-    resolution = flatten_image.shape[0]
-
-    # Unique value telling if a pixel is defined in the max tree or not.
-    undefined_node = resolution + 2
-
-    # We generate an extra vector of pixels that order nodes downard.
-    # This vector allow to traverse the tree both upward and downard
-    # without having to sort childrens of each node.
-    # Initially, we sort pixel by increasing value and add indices in it.
-    sorted_pixels = flatten_image.argsort()
-
-    # We store in the parent node of each pixel in an image.
-    # To do so we use the index of the pixel (x + y * width).
-    parents = np.full(
-        resolution,
-        fill_value=undefined_node,
-        dtype=np.uint32)
-
-    ranks = np.full(
-        resolution,
-        fill_value=0,
-        dtype=np.uint32)
-
-    reprs = np.full(
-        resolution,
-        fill_value=0,
-        dtype=np.uint32)
-
-    # zparents make root finding much faster.
-    zparents = parents.copy()
-
-    # We go through sorted pixels in the reverse order.
-    for pi in sorted_pixels[::-1]:
-        # Make a node.
-        # By default, a pixel is its own parent.
-        parents[pi] = pi
-        zparents[pi] = pi
-        ranks[pi] = 0
-        reprs[pi] = pi
-
-        zp = pi
-
-        # Find the row of this pixel.
-        pixel_row = math.floor(pi / width)
-
-        # We need to go through neighbors that already have a parent.
-        if connection8:
-            neighbors = get_8_neighbors(width, height, resolution, pi, pixel_row)
-        else:
-            neighbors = get_4_neighbors(width, height, resolution, pi, pixel_row)
-
-        # Filter neighbors.
-        neighbors = [n for n in neighbors if parents[n] != undefined_node]
-
-        # Go through neighbors.
-        for nei_pi in neighbors:
-            zn = find_pixel_parent(zparents, nei_pi)
-
-            if zn != zp:
-                parents[reprs[zn]] = pi
-
-                if ranks[zp] < ranks[zn]:
-                    # Swap them.
-                    zp, zn = zn, zp
-
-                # Merge sets.
-                zparents[zn] = zp
-                reprs[zp] = pi
-
-                if ranks[zp] == ranks[zn]:
-                    ranks[zp] += 1
-
-    canonize(flatten_image, parents, sorted_pixels)
-
-    return MaxTreeStructure(parents, sorted_pixels)
-
-
-
 
 @jit(nopython=True)
 def find_pixel_parent(parents, index):
@@ -150,80 +56,105 @@ def canonize(image, parents, nodes_order):
 
 
 @jit(nopython=True)
-def get_4_neighbors(width, height, resolution, pi, pixel_row):
+def get_neighbors(connexity, shape, pi, size):
     """
-    For a given image width, height and pixel index, return the index
-    of direct neighbor pixels using 4 connection.
+    :param connexity: connexity of the maxtree : acceptable value : 2D 4/8 - 3D 6/18/26 (default 4 and 6)
+    :param shape: shape of the input image
+    :param pi: current index in the flatten input image
+    :return: indexes of the neighbors with the given connexity and position
     """
-
-    top_pi = pi - width
-    bottom_pi = pi + width
-    left_pi = pi - 1
-    right_pi = pi + 1
 
     neighbors = []
 
-    if top_pi >= 0:
-        neighbors.append(top_pi)
-    if bottom_pi < resolution:
-        neighbors.append(bottom_pi) 
+    # 2D
+    if len(shape) == 2:
 
-    # For right and left pixels, we need to check if we moved to 
-    # the next row or not.
-    if math.floor(left_pi / width) == pixel_row:
-        neighbors.append(left_pi) 
-    if math.floor(right_pi / width) == pixel_row:
-        neighbors.append(right_pi)
+        # 4-connexity
+        pi_row = math.floor(pi / shape[1])
+        pi_top = pi - shape[1]
+        pi_bot = pi + shape[1]
+        pi_lft = pi - 1
+        pi_rgt = pi + 1
+
+        if pi_top >= 0:
+            neighbors.append(pi_top)
+        if pi_bot < size:
+            neighbors.append(pi_bot)
+        if math.floor(pi_lft / shape[1]) == pi_row:
+            neighbors.append(pi_lft)
+        if math.floor(pi_rgt / shape[1]) == pi_row:
+            neighbors.append(pi_rgt)
+
+        # 8-connexity
+        if connexity == 8:
+            pi_top_lef = pi_top - 1
+            pi_top_rgt = pi_top + 1
+            pi_bot_lef = pi_bot - 1
+            pi_bot_rgt = pi_bot + 1
+
+            if math.floor(pi_top_lef / shape[1]) == pi_row - 1:
+                neighbors.append(pi_top_lef)
+            if math.floor(pi_top_rgt / shape[1]) == pi_row - 1:
+                neighbors.append(pi_top_rgt)
+            if math.floor(pi_bot_lef / shape[1]) == pi_row + 1:
+                neighbors.append(pi_bot_lef)
+            if math.floor(pi_bot_rgt / shape[1]) == pi_row + 1:
+                neighbors.append(pi_bot_rgt)
+
+    # 3D
+    """
+    if len(shape) == 3:
+
+        # 6-connexity
+        pi_row = math.floor(pi / shape[1])
+        pi_slc = math.floor(pi / shape[2])
+
+        pi_far = pi + shape[2]
+        pi_cls = pi - shape[2]
+        pi_top = pi - shape[1]
+        pi_bot = pi + shape[1]
+        pi_lft = pi - 1
+        pi_rgt = pi + 1
+
+        if pi_top >= 0:
+            neighbors.append(pi_top)
+        if pi_bot < np.prod(shape):
+            neighbors.append(pi_bot)
+        if math.floor(pi_lft / shape[1]) == pi_row:
+            neighbors.append(pi_lft)
+        if math.floor(pi_rgt / shape[1]) == pi_row:
+            neighbors.append(pi_rgt)
+        if math.floor(pi_far / shape[2]) == pi_slc:
+            neighbors.append(pi_far)
+        if math.floor(pi_cls / shape[2]) == pi_slc:
+            neighbors.append(pi_cls)
+
+        # 18-connexity
+        if(connexity >= 18):
+            # /!\ Not Implemented Yet /!\
+            pass
+        # 26-connexity
+        if(connexity == 26):
+            # /!\ Not Implemented Yet /!\
+            pass
+    """
 
     return neighbors
 
 
 @jit(nopython=True)
-def get_8_neighbors(width, height, resolution, pi, pixel_row):
+def maxtree_berger_union_by_rank(input, connexity):
     """
-    For a given image width, height and pixel index, return the index
-    of direct neighbor pixels using 8 connection.
-    """
+    Union-find with union-by-rank based max-tree algorithm.
+    Algorithm 3 in the paper [1].
 
-    neighbors = get_4_neighbors(width, height, resolution, pi, pixel_row)
-
-    top_left_pi = pi - width  - 1
-    top_right_pi = top_left_pi + 2
-    bottom_left_pi = pi + width - 1
-    bottom_right_pi = bottom_left_pi + 2
-
-    if top_left_pi >= 0 and math.floor(top_left_pi / width) == pixel_row - 1:
-        neighbors.append(top_left_pi)
-
-    if top_right_pi >= 0 and math.floor(top_right_pi / width) == pixel_row - 1:
-        neighbors.append(top_right_pi)
-
-    if bottom_left_pi < resolution and math.floor(bottom_left_pi / width) == pixel_row + 1:
-        neighbors.append(bottom_left_pi)
-
-    if bottom_right_pi < resolution and math.floor(bottom_right_pi / width) == pixel_row + 1:
-        neighbors.append(bottom_right_pi)
-
-    return neighbors
-
-
-@jit(nopython=True)
-def maxtree_berger(image, connection8=True):
-    """
-    Union-find (without union-by-rank) based max-tree algorithm as proposed by Berger et al.
-
-    -> Algorithm 2 in the paper.
-
-    Arguments:
-    image is supposed to be a numpy array.
-
-    Returns:
+    :param input: numpy ndarray of a single channel image
+    :param connexity: connexity of the maxtree : acceptable value : 2D 4/8 - 3D 6/18/26 (default 4 and 6)
+    :return: the maxtree of the image (parent and S vector pair)
     """
 
-    (height, width) = (image.shape[0], image.shape[1])
-
-    flatten_image = image.flatten()
-    resolution = flatten_image.shape[0]
+    input_flat = input.flatten()
+    resolution = input_flat.size
 
     # Unique value telling if a pixel is defined in the max tree or not.
     undefined_node = resolution + 2
@@ -232,13 +163,23 @@ def maxtree_berger(image, connection8=True):
     # This vector allow to traverse the tree both upward and downard
     # without having to sort childrens of each node.
     # Initially, we sort pixel by increasing value and add indices in it.
-    sorted_pixels = flatten_image.argsort()
+    sorted_pixels = input_flat.argsort()
 
     # We store in the parent node of each pixel in an image.
     # To do so we use the index of the pixel (x + y * width).
     parents = np.full(
-        resolution, 
-        fill_value=undefined_node, 
+        resolution,
+        fill_value=undefined_node,
+        dtype=np.uint32)
+
+    ranks = np.full(
+        resolution,
+        fill_value=0,
+        dtype=np.uint32)
+
+    reprs = np.full(
+        resolution,
+        fill_value=0,
         dtype=np.uint32)
 
     # zparents make root finding much faster.
@@ -250,49 +191,52 @@ def maxtree_berger(image, connection8=True):
         # By default, a pixel is its own parent.
         parents[pi] = pi
         zparents[pi] = pi
+        ranks[pi] = 0
+        reprs[pi] = pi
 
-        # Find the row of this pixel.
-        pixel_row = math.floor(pi / width)
+        zp = pi
 
-        # We need to go through neighbors that already have a parent.
-        if connection8:
-            neighbors = get_8_neighbors(width, height, resolution, pi, pixel_row)
-        else:
-            neighbors = get_4_neighbors(width, height, resolution, pi, pixel_row)
+        neighbors = get_neighbors(connexity, input.shape, pi, input.size)
 
         # Filter neighbors.
         neighbors = [n for n in neighbors if parents[n] != undefined_node]
 
-        # Merge nodes together.
+        # Go through neighbors.
         for nei_pi in neighbors:
-            nei_root = find_pixel_parent(zparents, nei_pi)
+            zn = find_pixel_parent(zparents, nei_pi)
 
-            if nei_root != pi:
-                zparents[nei_root] = pi
-                parents[nei_root] = pi
+            if zn != zp:
+                parents[reprs[zn]] = pi
 
-    canonize(flatten_image, parents, sorted_pixels)
-    return parents, sorted_pixels
+                if ranks[zp] < ranks[zn]:
+                    # Swap them.
+                    zp, zn = zn, zp
 
+                # Merge sets.
+                zparents[zn] = zp
+                reprs[zp] = pi
+
+                if ranks[zp] == ranks[zn]:
+                    ranks[zp] += 1
+
+    canonize(input_flat, parents, sorted_pixels)
+
+    return MaxTreeStructure(parents, sorted_pixels)
 
 
 @jit(nopython=True)
-def maxtree_union_find_level_compression(image, connection8=True):
+def maxtree_berger_union_by_rank_level_compression(input, connexity):
     """
-    Union-find with level compression.
+    Union-find with union-by-rank based max-tree algorithm with level compression.
+    Algorithm 5 in the paper [1].
 
-    -> Algorithm 5 in the paper.
-
-    Arguments:
-    image is supposed to be a numpy array.
-
-    Returns:
+    :param input: numpy ndarray of a single channel image
+    :param connexity: connexity of the maxtree : acceptable value : 2D 4/8 - 3D 6/18/26 (default 4 and 6)
+    :return: the maxtree of the image (parent and S vector pair)
     """
 
-    (height, width) = (image.shape[0], image.shape[1])
-
-    flatten_image = image.flatten()
-    resolution = flatten_image.shape[0]
+    input_flat = input.flatten()
+    resolution = input_flat.size
 
     # Unique value telling if a pixel is defined in the max tree or not.
     undefined_node = resolution + 2
@@ -301,7 +245,7 @@ def maxtree_union_find_level_compression(image, connection8=True):
     # This vector allow to traverse the tree both upward and downard
     # without having to sort childrens of each node.
     # Initially, we sort pixel by increasing value and add indices in it.
-    sorted_pixels = flatten_image.argsort()
+    sorted_pixels = input_flat.argsort()
 
     # We store in the parent node of each pixel in an image.
     # To do so we use the index of the pixel (x + y * width).
@@ -324,14 +268,7 @@ def maxtree_union_find_level_compression(image, connection8=True):
 
         zp = pi
 
-        # Find the row of this pixel.
-        pixel_row = math.floor(pi / width)
-
-        # We need to go through neighbors that already have a parent.
-        if connection8:
-            neighbors = get_8_neighbors(width, height, resolution, pi, pixel_row)
-        else:
-            neighbors = get_4_neighbors(width, height, resolution, pi, pixel_row)
+        neighbors = get_neighbors(connexity, input.shape, pi, input.size)
 
         # Filter neighbors.
         neighbors = [n for n in neighbors if parents[n] != undefined_node]
@@ -341,7 +278,7 @@ def maxtree_union_find_level_compression(image, connection8=True):
             zn = find_pixel_parent(zparents, nei_pi)
 
             if zn != zp:
-                if flatten_image[zp] == flatten_image[zn]:
+                if input_flat[zp] == input_flat[zn]:
                     zp, zn = zn, zp
 
                 # Merge sets.
@@ -351,9 +288,9 @@ def maxtree_union_find_level_compression(image, connection8=True):
                 sorted_pixels[j] = zn
                 j -= 1
 
-    canonize(flatten_image, parents, sorted_pixels)
+    canonize(input_flat, parents, sorted_pixels)
 
-    return parents, sorted_pixels
+    return MaxTreeStructure(parents, sorted_pixels)
 
 
 @jit(nopython=True)
@@ -388,7 +325,7 @@ def maxtree(input, connexity=None):
     # maxtree_salembier(input, connexity)
 
     # High quantization : Berger + rank
-    return maxtree_berger_rank(input, connexity)
+    return maxtree_berger_union_by_rank_level_compression(input, connexity)
 
 
 @jit(nopython=True)
@@ -530,7 +467,6 @@ image_input = image_read("examples/images/circuit_small.png")
 # image_input = image_input / image_input.max()
 # image_output = area_filter(image_input, 500)
 image_output = contrast_filter(image_input, 1)
-
 
 import matplotlib.pyplot as plt
 plt.imshow(image_input, cmap="gray")
