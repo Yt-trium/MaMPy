12e9737e134624feedd3d0ef63f5384ffe3c4daa
Yt-trium <cyril.meyer.68@gmail.com>
2019-01-18 14:42:57 +0100

    create file for future implementation (tree of shape and binary partition tree)

---
+++ binary_partition_tree.py
@@ -0,0 +1,11 @@
+"""
+Binary Partition Tree computation and filters.
+
+Reference:
+    [1] Binary Partition Tree as an Efficient Representation
+    for Image Processing, Segmentation, and Information Retrieval
+    Philippe Salembier, Luis Garrido.
+
+Authors:
+C. Meyer
+"""
---
+++ shape_tree.py
@@ -0,0 +1,11 @@
+"""
+Tree of Shape computation and filters.
+
+Reference:
+    [1] A quasi-linear algorithm to compute the tree of shapes of n-D images
+    Thierry Géraud, Edwin Carlinet, Sébastien Crozet, Laurent Najman.
+
+Authors:
+C. Meyer
+"""
+
