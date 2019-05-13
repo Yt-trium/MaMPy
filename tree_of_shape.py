221c2197f48c956b64310ce9f24d69e35d2154f2
Yt-trium <cyril.meyer.68@gmail.com>
2019-05-13 14:35:36 +0200

    immersion seems done

--- tree_of_shape.py
+++ tree_of_shape.py
@@ -66,10 +66,10 @@ def interpolate2D(input, interpolationMode):
     output = np.zeros(tuple([2 * x - 1 for x in input.shape]), dtype=input.dtype)
 
     # select interpolation function
-    if InterpolationMode == InterpolationMode.MAX:
-        interpolationFunc = np.min
-    elif InterpolationMode == InterpolationMode.MIN:
+    if interpolationMode == InterpolationMode.MAX:
         interpolationFunc = np.max
+    elif interpolationMode == InterpolationMode.MIN:
+        interpolationFunc = np.min
     else:
         interpolationFunc = np.median
 
@@ -98,10 +98,41 @@ def immersion2D(input):
     :return: numpy 2d array of the immersed image
     '''
 
-    
-    return 0
+    # check input validity
+    assert type(input) == np.ndarray
+    assert input.ndim == 2
 
-print(immersion2D(np.array([[1, 2], [3, 4]])))
+    # create output
+    output = np.zeros(tuple([2 * x - 1 for x in input.shape]), dtype=object)
+
+    for x in range(input.shape[0]):
+        for y in range(input.shape[1]):
+            # copy input
+            output[2 * x][2 * y] = input[x][y]
+
+            if(x < input.shape[0]-1):
+                # interpolation x + 1
+                output[2 * x + 1][2 * y] = (np.min([input[x][y], input[x + 1][y]]),
+                                            np.max([input[x][y], input[x + 1][y]]))
+            if(y < input.shape[1]-1):
+                # interpolation y + 1
+                output[2 * x][2 * y + 1] = (np.min([input[x][y], input[x][y + 1]]),
+                                            np.max([input[x][y], input[x][y + 1]]))
+
+            if(x < input.shape[0]-1 and y < input.shape[1]-1):
+                # interpolation x + 1 and y + 1
+                output[2 * x + 1][2 * y + 1] = (np.min([input[x][y], input[x + 1][y],
+                                                        input[x][y + 1], input[x + 1][y + 1]]),
+                                                np.max([input[x][y], input[x + 1][y],
+                                                        input[x][y + 1], input[x + 1][y + 1]]),)
+
+    return output
+
+print(addBorderMedian(np.array([[1, 2], [3, 4]])))
+print((np.array([[1, 2], [3, 4]])))
+print(interpolate2D(np.array([[1, 2], [3, 4]]), InterpolationMode.MAX))
+print(immersion2D(interpolate2D(np.array([[1, 2], [3, 4]]), InterpolationMode.MAX)))
+
 
 def interpolate_and_immerse_2D(imput, interpolationMode):
     '''
@@ -109,4 +140,5 @@ def interpolate_and_immerse_2D(imput, interpolationMode):
     :param interpolationMode: the interpolation mode (max, min or median)
     :return: numpy 2d array of the Khalimsky grid
     '''
+
     return immersion2D(interpolate2D(addBorderMedian(input)))
